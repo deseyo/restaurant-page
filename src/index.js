4567bfff75d204e105900fa2a614a76c2951e405
deseyo <156849439+deseyo@users.noreply.github.com>
2024-08-11 17:32:31 +0200

    Set logic to change webpage tabs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import './style.css'
+import './style.css';
+import { initHome } from './js/home';
+import { initMenu } from './js/menu';
+import { initContact } from './js/contact';
+
+const home = document.querySelector('#home');
+const menu = document.querySelector('#menu');
+const contact = document.querySelector('#contact');
+const content = document.querySelector('#content');
+
+content.appendChild(initHome());
+
+home.addEventListener('click', () => {
+  content.innerHTML = '';
+  content.appendChild(initHome())
+})
+
+menu.addEventListener('click', () => {
+  content.innerHTML = '';
+  content.appendChild(initMenu())
+})
+
+contact.addEventListener('click', () => {
+  content.innerHTML = '';
+  content.appendChild(initContact());
+})
